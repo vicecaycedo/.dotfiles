2d7e97384a0fb18cca86a8c87b85dbe10242c16d
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-09-25 16:17:29 -0400

    Neovim: Add smart mappings for Flutter Outline toggle.

--- nvim-te/.config/nvim/lua/te/mappings.lua
+++ nvim-te/.config/nvim/lua/te/mappings.lua
@@ -1,10 +1,37 @@
 local keys = require('which-key')
 
+local function smart_outline()
+  local ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  if ft == 'dart' then
+    vim.cmd('FlutterOutlineToggle')
+  else
+    vim.cmd('SymbolsOutline')
+  end
+end
+
+local t = require('vc.util').replace_termcodes
+function _G.smart_esc()
+  local ft = vim.api.nvim_buf_get_option(0, 'filetype')
+  if ft == 'flutterToolsOutline' then
+    return t'<Cmd>FlutterOutlineToggle<CR>'
+  else
+    return t'<Esc>'
+  end
+end
+
+local remap = vim.api.nvim_set_keymap
+remap('n', '<Esc>', 'v:lua.smart_esc()', { expr = true, noremap = true, })
+
 keys.register {
   ['<leader>'] = {
     F = {
       [[<Cmd>Telescope flutter commands<CR>]],
       'flutter',
     },
-  }
+  },
+  -- Override default SymbolsOutline mapping.
+  ['<leader>f'] = {
+    s = { smart_outline, 'explore symbols' },
+  },
 }
+
