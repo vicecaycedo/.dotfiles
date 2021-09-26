b3ecf0357f428f02b7faa595e3861b654e033780
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-09-26 11:17:24 -0400

    Neovim: Change double quotes to single quotes.

--- nvim/.config/nvim/lua/vc/config/kommentary.lua
+++ nvim/.config/nvim/lua/vc/config/kommentary.lua
@@ -1,6 +1,8 @@
-require('kommentary.config').configure_language('dart', {
-    single_line_comment_string = "//",
-    multi_line_comment_strings = {"/*", "*/"},
+local kommentary_config = require('kommentary.config')
+
+kommentary_config.configure_language('dart', {
+    single_line_comment_string = '//',
+    multi_line_comment_strings = {'/*', '*/'},
     prefer_single_line_comments = true,
 })
 
