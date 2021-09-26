993271ad6fc0185067f1063d4be028f03fb2ae47
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-09-26 11:17:40 -0400

    Neovim: Configure kommentary for Lua.

--- nvim/.config/nvim/lua/vc/config/kommentary.lua
+++ nvim/.config/nvim/lua/vc/config/kommentary.lua
@@ -6,3 +6,9 @@ kommentary_config.configure_language('dart', {
     prefer_single_line_comments = true,
 })
 
+kommentary_config.configure_language('lua', {
+    single_line_comment_string = '--',
+    multi_line_comment_strings = {'--[[', ']]'},
+    prefer_single_line_comments = true,
+})
+
