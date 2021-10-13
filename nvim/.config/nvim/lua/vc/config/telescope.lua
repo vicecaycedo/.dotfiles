251c8bd951777e1fbfc1204468d709bd30c226f8
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-10-13 02:17:24 +0000

    Neovim: Use ivy theme for buffers, finders, and history pickers.

--- nvim/.config/nvim/lua/vc/config/telescope.lua
+++ nvim/.config/nvim/lua/vc/config/telescope.lua
@@ -3,18 +3,22 @@ local actions = require('telescope.actions')
 telescope.setup {
   defaults = {
     file_ignore_patterns = { 'packer_compiled.*' },
-    layout_config = {
-      prompt_position = 'top',
-    },
     mappings = {
-      i = {
-        -- ['<Esc>'] = actions.close,
-      },
       n = {
         v = actions.file_vsplit,
       },
     },
-    sorting_strategy = 'ascending',
+  },
+  pickers = {
+    buffers = {
+      theme = 'ivy',
+    },
+    find_files = {
+      theme = 'ivy',
+    },
+    old_files = {
+      theme = 'ivy',
+    },
   },
 }
 telescope.load_extension('fzf')
