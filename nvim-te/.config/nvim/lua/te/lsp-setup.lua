a6b40f7ad083be122fad14a445cca2016a312fe2
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-10-14 14:40:25 +0000

    Neovim: Move LSP to core (vc).

--- nvim-te/.config/nvim/lua/te/lsp-setup.lua
+++ nvim-te/.config/nvim/lua/te/lsp-setup.lua
@@ -3,11 +3,13 @@
 --     dart (bundled with Flutter)
 --   Through nvim-lspinstall (`:LspInstall <server>`):
 --     angular, bash, css, html, json, lua, python, vim, yaml
+
+-- TODO: Refactor now that LSP has been moved to core (vc).
+
 -- Configure how diagnostics are displayed.
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
-    -- underline = false,
   })
 
 local on_attach = function(client)
@@ -17,7 +19,7 @@ local on_attach = function(client)
       [[
       aug lsp_format
         au! * <buffer>
-        au BufWritePre <buffer> lua require('te.lsp-util').format_buffer()
+        au BufWritePre <buffer> lua require('vc.lsp-util').format_buffer()
       aug END
     ]])
   end
@@ -29,69 +31,10 @@ local on_attach = function(client)
       [[
       aug lsp_organize_imports
         au! * <buffer>
-        au BufWritePre <buffer> lua require('te.lsp-util').organize_imports()
+        au BufWritePre <buffer> lua require('vc.lsp-util').organize_imports()
       aug END
     ]])
   end
-end
-
-local lsp_settings = {
-  lua = {
-    Lua = {
-      runtime = {
-        path = vim.split(package.path, ';'),
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {
-          'vim', -- Neovim
-          'use', -- Packer
-        },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files.
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-        },
-      },
-    },
-  },
-}
-
-local function server_config(server)
-  local config = {
-    on_attach = on_attach,
-  }
-
-  if server == 'lua' then
-    config.settings = lsp_settings.lua
-  end
-
-  return config
-end
-
--- Loads installed servers.
-local function setup_servers()
-  local lsp_install = require('lspinstall')
-  lsp_install.setup()
-
-  local servers = lsp_install.installed_servers()
-  for _, server in pairs(servers) do
-    local config = server_config(server)
-    require('lspconfig')[server].setup(config)
-  end
-end
-
--- Load installed servers.
-setup_servers()
-
--- Automatically reload installed servers after `:LspInstall <server>`.
-require('lspinstall').post_install_hook = function()
-  -- Reload installed servers.
-  setup_servers()
-  -- Trigger FileType autocommands that start the servers.
-  vim.cmd([[bufdo e]])
 end
 
 -- Manage Flutter LSP separately.
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
+require('vc.lsp-setup')
 require('vc.mappings')
-require('vc.plugins')
 require('vc.options')
+require('vc.plugins')
 
 local function config_exists(name)
   local config_path = vim.fn.stdpath('config')
