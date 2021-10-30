cdd98afee859bab3a466e870219ca728e9ed33e9
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-10-30 16:53:36 -0400

    Neovim: Remove Packer bootstrapping.

--- nvim/.config/nvim/lua/vc/plugins.lua
+++ nvim/.config/nvim/lua/vc/plugins.lua
@@ -1,15 +1,3 @@
--- Bootstrap Packer.
-local install_path = vim.fn.stdpath('data')
-  .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({
-    'git',
-    'clone',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path,
-  })
-end
-
 -- Initialize Packer.
 local packer = require('packer')
 packer.init()
