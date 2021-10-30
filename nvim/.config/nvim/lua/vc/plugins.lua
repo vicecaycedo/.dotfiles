8196b19b6f7c20bf3bd2877ff465dedd179c8923
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-10-30 16:53:11 -0400

    Neovim: Use Packer custom initialization.

--- nvim/.config/nvim/lua/vc/plugins.lua
+++ nvim/.config/nvim/lua/vc/plugins.lua
@@ -10,152 +10,159 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   })
 end
 
-require('packer').startup(function(use)
-  use('bkad/camelcasemotion') -- Camelcase and underscore objects.
-  use('neovim/nvim-lspconfig') -- LSP.
-  use('williamboman/nvim-lsp-installer') -- Language server installer.
-  use('wbthomason/packer.nvim') -- Package manager.
-  use('tpope/vim-abolish') -- Support word variants.
-  use('moll/vim-bbye') -- Keep layout when closing buffers.
-  use('michaeljsmith/vim-indent-object') -- Indent objects.
-  use('tpope/vim-repeat') -- Repeat plugin commands.
-  use('tpope/vim-surround') -- Surroundings.
-  use('folke/which-key.nvim') -- Keybindings.
+-- Initialize Packer.
+local packer = require('packer')
+packer.init()
+packer.reset()
 
-  -- Flutter.
-  use({ 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' })
+-- Load plugins.
+local use = packer.use
+use('bkad/camelcasemotion') -- Camelcase and underscore objects.
+use('neovim/nvim-lspconfig') -- LSP client configurations.
+use('wbthomason/packer.nvim') -- Package manager.
+use('tpope/vim-abolish') -- Support word variants.
+use('moll/vim-bbye') -- Keep layout when closing buffers.
+use('michaeljsmith/vim-indent-object') -- Indent objects.
+use('tpope/vim-repeat') -- Repeat plugin commands.
+use('tpope/vim-surround') -- Surroundings.
+use('folke/which-key.nvim') -- Keybindings.
 
-  -- Toggle comments.
-  use({
-    'b3nj5m1n/kommentary',
-    config = function()
-      require('vc.config.kommentary')
-    end,
-  })
+-- Toggle comments.
+use({
+  'b3nj5m1n/kommentary',
+  config = function()
+    require('vc.config.kommentary')
+  end,
+})
 
-  -- Statusline.
-  use({
-    'hoob3rt/lualine.nvim',
-    config = function()
-      require('vc.config.lualine')
-    end,
-  })
+-- Statusline.
+use({
+  'hoob3rt/lualine.nvim',
+  config = function()
+    require('vc.config.lualine')
+  end,
+})
 
-  -- General purpose language server.
-  use({
-    'jose-elias-alvarez/null-ls.nvim',
-    requires = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  })
+-- Autocompletion and Snippets.
+use({
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+  },
+  setup = function()
+    require('vc.setup.vsnip')
+  end,
+  config = function()
+    require('vc.config.nvim-cmp')
+  end,
+})
 
-  -- Autocompletion and Snippets.
-  use({
-    'hrsh7th/nvim-cmp',
-    requires = {
-      'neovim/nvim-lspconfig',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-vsnip',
-      'hrsh7th/vim-vsnip',
-    },
-    setup = function()
-      require('vc.setup.vsnip')
-    end,
-    config = function()
-      require('vc.config.nvim-cmp')
-    end,
-  })
+-- Language server installer.
+use({
+  'williamboman/nvim-lsp-installer',
+  requires = 'neovim/nvim-lspconfig',
+})
 
-  -- File explorer.
-  use({
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    cmd = 'NvimTreeToggle',
-    setup = function()
-      require('vc.setup.nvim-tree')
-    end,
-    config = function()
-      require('vc.config.nvim-tree')
-    end,
-  })
+-- General purpose language server.
+use({
+  'jose-elias-alvarez/null-ls.nvim',
+  requires = {
+    'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
+  },
+})
 
-  -- Treesitter.
-  use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    event = 'BufRead',
-    config = function()
-      require('vc.config.treesitter')
-    end,
-  })
+-- File explorer.
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  cmd = 'NvimTreeToggle',
+  setup = function()
+    require('vc.setup.nvim-tree')
+  end,
+  config = function()
+    require('vc.config.nvim-tree')
+  end,
+})
 
-  -- Icons.
-  use({
-    'kyazdani42/nvim-web-devicons',
-    module = 'nvim-web-devicons',
-  })
+-- Treesitter.
+use({
+  'nvim-treesitter/nvim-treesitter',
+  run = ':TSUpdate',
+  event = 'BufRead',
+  config = function()
+    require('vc.config.treesitter')
+  end,
+})
 
-  -- VCS sign column.
-  use({
-    'mhinz/vim-signify',
-    setup = function()
-      require('vc.setup.signify')
-    end,
-  })
+-- Icons.
+use({
+  'kyazdani42/nvim-web-devicons',
+  module = 'nvim-web-devicons',
+})
 
-  -- Startify.
-  use({
-    'mhinz/vim-startify',
-    setup = function()
-      require('vc.setup.startify')
-    end,
-  })
+-- VCS sign column.
+use({
+  'mhinz/vim-signify',
+  setup = function()
+    require('vc.setup.signify')
+  end,
+})
 
-  -- Toggle True/False.
-  use({
-    'andrewradev/switch.vim',
-    cmd = 'Switch',
-    setup = function()
-      require('vc.setup.switch')
-    end,
-  })
+-- Startify.
+use({
+  'mhinz/vim-startify',
+  setup = function()
+    require('vc.setup.startify')
+  end,
+})
 
-  -- Symbols outline.
-  use({
-    'simrat39/symbols-outline.nvim',
-    cmd = 'SymbolsOutline',
-    setup = function()
-      require('vc.setup.symbols-outline')
-    end,
-  })
+-- Theme.
+use({
+  'mofiqul/vscode.nvim',
+  setup = function()
+    vim.g.vscode_style = 'dark'
+  end,
+  config = function()
+    vim.cmd('colorscheme vscode')
+  end,
+})
 
-  -- Telescope.
-  use({
-    'nvim-telescope/telescope.nvim',
-    config = function()
-      require('vc.config.telescope')
-    end,
-    requires = {
-      'nvim-lua/plenary.nvim',
-      'nvim-lua/popup.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-    },
-  })
+-- Toggle True/False.
+use({
+  'andrewradev/switch.vim',
+  cmd = 'Switch',
+  setup = function()
+    require('vc.setup.switch')
+  end,
+})
 
-  -- Themes.
-  use({
-    'morhetz/gruvbox',
-    setup = function()
-      vim.g.gruvbox_italic = 1
-      vim.g.gruvbox_invert_selection = 0
-    end,
-  })
-  use({
-    'mofiqul/vscode.nvim',
-    config = function()
-      vim.cmd('colorscheme vscode')
-    end,
-    setup = function()
-      vim.g.vscode_style = 'dark'
-    end,
-  })
-end)
+-- Symbols outline.
+use({
+  'simrat39/symbols-outline.nvim',
+  cmd = 'SymbolsOutline',
+  setup = function()
+    require('vc.setup.symbols-outline')
+  end,
+})
+
+-- Telescope.
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'nvim-lua/popup.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+  },
+  config = function()
+    require('vc.config.telescope')
+  end,
+})
+
+-- TODO: Move to TE config.
+-- Flutter.
+use({ 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' })
