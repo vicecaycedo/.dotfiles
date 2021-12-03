3bab4c35850ab3996c3762b572c942aeaec19cdc
Vicente Caycedo <vicentecaycedo@gmail.com>
2021-12-03 13:40:17 +0000

    Neovim: Remove nvim-tree lazy loading so that FindFile can work on the first open.

--- nvim/.config/nvim/lua/vc/plugins.lua
+++ nvim/.config/nvim/lua/vc/plugins.lua
@@ -78,7 +78,6 @@ use({
 use({
   'kyazdani42/nvim-tree.lua',
   requires = { { 'kyazdani42/nvim-web-devicons', opt = true } },
-  cmd = { 'NvimTreeToggle', 'NvimTreeFindFile' },
   setup = function()
     require('vc.setup.nvim-tree')
   end,
