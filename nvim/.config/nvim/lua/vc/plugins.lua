bf10178d810f0b86ac63185e6803304496927e11
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-02-14 16:23:25 +0000

    Revert "Neovim: Add signature help completion plugin."
    This reverts commit 6449dc4233b564a0907e41f21176b5f216f16b71.

--- nvim/.config/nvim/lua/vc/plugins.lua
+++ nvim/.config/nvim/lua/vc/plugins.lua
@@ -67,7 +67,6 @@ use({
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
     'hrsh7th/cmp-nvim-lua',
     'hrsh7th/cmp-vsnip',
     'hrsh7th/vim-vsnip',
