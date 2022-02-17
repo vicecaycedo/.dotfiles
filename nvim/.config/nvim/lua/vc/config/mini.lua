4b86e68f4d353eab5a1e3f2f02713dddd437e4dd
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-02-17 16:41:12 +0000

    Neovim: Add mini plugin for auto session write.

---
+++ nvim/.config/nvim/lua/vc/config/mini.lua
@@ -0,0 +1,2 @@
+-- Sessions.
+require('mini.sessions').setup()
--- nvim/.config/nvim/lua/vc/plugins.lua
+++ nvim/.config/nvim/lua/vc/plugins.lua
@@ -38,6 +38,14 @@ use({
   end,
 })
 
+-- Collection of misc. plugins (e.g. sessions).
+use({
+  'echasnovski/mini.nvim',
+  config = function()
+    require('vc.config.mini')
+  end,
+})
+
 -- General purpose language server.
 use({
   'jose-elias-alvarez/null-ls.nvim',
