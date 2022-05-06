a2984e32fb77f8a906a5a15fe3f881e862e27030
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-05-06 16:28:56 +0000

    Neovim: Don't use Tab for autocompletion.

--- nvim/.config/nvim/lua/vc/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/vc/config/nvim-cmp.lua
@@ -1,14 +1,5 @@
 local cmp = require('cmp')
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-        :sub(col, col)
-        :match('%s')
-      == nil
-end
-
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(
     vim.api.nvim_replace_termcodes(key, true, true, true),
@@ -18,21 +9,15 @@ local feedkey = function(key, mode)
 end
 
 local smart_tab = function(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
-  elseif vim.fn['vsnip#available'](1) == 1 then
+  if vim.fn['vsnip#available'](1) == 1 then
     feedkey('<Plug>(vsnip-expand-or-jump)', '')
-  elseif has_words_before() then
-    cmp.complete()
   else
     fallback()
   end
 end
 
 local smart_s_tab = function(fallback)
-  if cmp.visible() then
-    cmp.select_prev_item()
-  elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+  if vim.fn['vsnip#jumpable'](-1) == 1 then
     feedkey('<Plug>(vsnip-jump-prev)', '')
   else
     fallback()
@@ -46,7 +31,7 @@ cmp.setup({
     end,
   },
   mapping = {
-    -- Source: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings.
+    -- Inspired by: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings.
     ['<Tab>'] = smart_tab,
     ['<S-Tab>'] = smart_s_tab,
     ['<CR>'] = cmp.mapping.confirm(),
