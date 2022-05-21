edf7ae89822275a8e04e5da76258047c2761dbd5
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-05-21 19:39:34 +0000

    Neovim: Remove stray VimL comment delimiters.

--- nvim/.config/nvim/after/ftplugin/help.lua
+++ nvim/.config/nvim/after/ftplugin/help.lua
@@ -4,8 +4,8 @@ vim.o.relativenumber = true
 vim.b.undo_ftplugin = vim.b.undo_ftplugin .. ' | setlocal number<'
 vim.b.undo_ftplugin = vim.b.undo_ftplugin .. ' | setlocal relativenumber<'
 
--- " Use <CR> to jump to the help menu entry for the tag under the cursor.
+-- Use <CR> to jump to the help menu entry for the tag under the cursor.
 vim.keymap.set('n', '<CR>', '<C-]>', { buffer = true })
 
--- " Use <Esc> to close the help menu.
+-- Use <Esc> to close the help menu.
 vim.keymap.set('n', '<Esc>', '<Cmd>bdelete<CR>', { buffer = true })
