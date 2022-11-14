92c2824ef39c11988d5fc4733531a6dd3cc9bcb6
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-11-14 14:09:52 +0000

    Neovim: Remove pudb snippet.

--- nvim/.config/nvim/snippets/bzl.lua
+++ nvim/.config/nvim/snippets/bzl.lua
@@ -2,5 +2,4 @@
 return {
   s('td', fmt('# TODO({}): {}', { i(1, 'vicentecaycedo'), i(0, '...') })),
   s('log', t('"//third_party/py/absl/logging",')),
-  s('pudb', t('"//third_party/py/pudb",  # build-cleaner: keep')),
 }
