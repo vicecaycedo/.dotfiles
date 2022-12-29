ca052864420446f9f608359497eb72090ef969cc
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-12-29 17:46:37 +0000

    Neovim: Use global status line.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -11,6 +11,7 @@ return {
     options = {
       component_separators = { left = '|', right = '|' },
       section_separators = { left = '', right = '' },
+      globalstatus = true,
     },
     sections = {
       lualine_b = { 'diagnostics' },
