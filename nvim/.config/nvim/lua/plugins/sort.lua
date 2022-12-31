ee58c81252b99d9f31d703e662c932a740390dcd
Vicente Caycedo <vicentecaycedo@gmail.com>
2022-12-31 20:12:21 +0000

    Neovim: Configure vim-sort-motion to remove duplicates when sorting.

--- nvim/.config/nvim/lua/plugins/sort.lua
+++ nvim/.config/nvim/lua/plugins/sort.lua
@@ -1,6 +1,10 @@
 return {
   'christoomey/vim-sort-motion',
   event = 'VeryLazy',
+  init = function()
+    -- Remove duplicates when sorting.
+    vim.g.sort_motion_flags = 'u'
+  end,
   config = function()
     -- Register mapping with which-key.
     local loaded, wk = pcall(require, 'which-key')
