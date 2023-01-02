57c4845e5de6a1de9c4d07d62b042982d95350c4
Vicente Caycedo <vicentecaycedo@gmail.com>
2023-01-02 19:35:24 +0000

    Neovim: Change Signify mappings to restore default join mapping.

--- nvim/.config/nvim/lua/plugins/signs.lua
+++ nvim/.config/nvim/lua/plugins/signs.lua
@@ -6,8 +6,8 @@ return {
   end,
   config = function()
     -- Set keymaps.
-    nmap('J', '<Plug>(signify-next-hunk)zz', 'Next diff')
-    nmap('K', '<Plug>(signify-prev-hunk)zz', 'Previous diff')
+    nmap('L', '<Plug>(signify-next-hunk)zz', 'Next diff')
+    nmap('H', '<Plug>(signify-prev-hunk)zz', 'Previous diff')
     nmap('<Leader>md', '<Cmd>SignifyHunkDiff<CR>', 'Hunk diff')
     nmap('<Leader>mu', '<Cmd>SignifyHunkUndo<CR>', 'Hunk undo')
   end,
