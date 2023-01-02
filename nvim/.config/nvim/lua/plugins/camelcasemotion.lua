367e626d329f73c99127f40e291d2c0ec7a8ee06
Vicente Caycedo <vicentecaycedo@gmail.com>
2023-01-02 21:06:30 +0000

    Neovim: Move blaze mappings under <Leader>b.

--- nvim/.config/nvim/lua/plugins/camelcasemotion.lua
+++ nvim/.config/nvim/lua/plugins/camelcasemotion.lua
@@ -5,12 +5,14 @@ return {
     vim.g.camelcasemotion_key = '<Leader>'
   end,
   config = function()
+    -- Remove default mapping for <Leader>b.
+    vim.cmd.unmap('<Leader>b')
+
     -- Ignore mappings in WhichKey.
     local loaded, wk = pcall(require, 'which-key')
     if loaded then
       local ignore = 'which_key_ignore'
       wk.register({
-        ['<Leader>b'] = ignore,
         ['<Leader>e'] = ignore,
         ['<Leader>ge'] = ignore,
         ['<Leader>w'] = ignore,
