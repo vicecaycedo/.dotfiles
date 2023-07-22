f53d53689f77d0cda7fb634eefa3cf7e3e375ff9
Vicente Caycedo <vicentecaycedo@gmail.com>
2023-07-22 09:58:49 -0400

    Neovim: Sort plugins in plugins/init.lua.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,9 +1,9 @@
 return {
-  -- Extend |gf| support for Lua files.
-  { 'sam4llis/nvim-lua-gf', ft = 'lua' },
-
   -- Manage pairs (e.g. parentheses, brackets, etc.) automatically.
   { 'windwp/nvim-autopairs', config = true },
+
+  -- Extend |gf| support for Lua files.
+  { 'sam4llis/nvim-lua-gf', ft = 'lua' },
 
   -- Add/change/delete surrounding delimiters.
   { 'kylechui/nvim-surround', event = 'VeryLazy', config = true },
