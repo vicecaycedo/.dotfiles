491e3a1b685a7e739fc4ac901812394b9c9fff15
Vicente Caycedo <vicentecaycedo@gmail.com>
2023-07-24 06:55:49 -0400

    Neovim: Simplify lualine separators configuration.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,8 +9,8 @@ return {
       'symbols-outline',
     },
     options = {
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      component_separators = '',
+      section_separators = '',
       globalstatus = true,
     },
     sections = {
