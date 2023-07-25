530dc7592476afdde296a04e4d254c9e00ac7b3e
Vicente Caycedo <vicentecaycedo@gmail.com>
2023-07-25 17:25:04 +0000

    Neovim: Remove gruvbox colorscheme.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,15 +7,4 @@ return {
       vim.cmd.colorscheme('vscode')
     end,
   },
-  -- Other colorschemes.
-  -- These are automatically loaded with the |colorscheme| Ex command.
-  {
-    'morhetz/gruvbox',
-    lazy = true,
-    config = function()
-      vim.g.gruvbox_contrast_dark = 'hard'
-      vim.g.gruvbox_invert_selection = false
-      vim.g.gruvbox_sign_column = 'bg0'
-    end,
-  },
 }
