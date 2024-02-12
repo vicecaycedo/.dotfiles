77ab123cfa2d3daa07462dc0e677a30d7a6bc4db
Vicente Caycedo <vicentecaycedo@gmail.com>
2024-02-12 18:09:40 +0000

    Neovim: Add snippets for C++.

---
+++ nvim/.config/nvim/snippets/cpp.lua
@@ -0,0 +1,5 @@
+---@diagnostic disable:undefined-global
+return {
+  s('td', fmt('// TODO({}): {}', { i(1, 'vicentecaycedo'), i(0, '...') })),
+  s('p', fmt('std::cout << {} << std::endl;', { i(0) })),
+}
