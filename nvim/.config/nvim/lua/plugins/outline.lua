7d460a32609d0322f56e3c0b506f5228052cfd5f
Vicente Caycedo <vicentecaycedo@gmail.com>
2024-02-17 19:27:13 -0500

    Revert "Neovim: Remove symbols outline plugin."
    This reverts commit a599c5cd0a5da955512980feda91be67f6adb6a4.

---
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,13 @@
+return {
+  'simrat39/symbols-outline.nvim',
+  keys = {
+    { '<Leader>fs', '<Cmd>SymbolsOutline<CR>', desc = 'Explore symbols' },
+  },
+  opts = {
+    show_guides = false,
+    show_numbers = true,
+    show_relative_numbers = true,
+    show_symbol_details = false,
+    width = 50,
+  },
+}
