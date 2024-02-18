f74c74ca5fc664c9373c61218d4ef190bfb62350
Vicente Caycedo <vicentecaycedo@gmail.com>
2024-02-17 19:27:10 -0500

    Neovim: Add autopairs plugin.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,6 +2,9 @@ return {
   -- Extend |gf| support for Lua files.
   { 'sam4llis/nvim-lua-gf', ft = 'lua' },
 
+  -- Manage pairs (e.g. parentheses, brackets, etc.) automatically.
+  { 'windwp/nvim-autopairs', config = true },
+
   -- Add/change/delete surrounding delimiters.
   { 'kylechui/nvim-surround', event = 'VeryLazy', config = true },
 
