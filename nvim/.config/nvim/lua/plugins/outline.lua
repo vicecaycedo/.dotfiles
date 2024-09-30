75cb66ec1cfa9925499b89e564df4fe4848ee442
Vicente Caycedo <vicentecaycedo@gmail.com>
2024-09-30 14:10:24 +0000

    Neovim: Fix outline window config.

--- nvim/.config/nvim/lua/plugins/outline.lua
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -4,9 +4,11 @@ return {
     { '<Leader>fs', '<Cmd>Outline<CR>', desc = 'toggle outline' },
   },
   opts = {
-    show_numbers = true,
-    show_relative_numbers = true,
-    width = 100,
+    outline_window = {
+      show_numbers = true,
+      show_relative_numbers = true,
+      width = 50,
+    },
     symbol_folding = {
       autofold_depth = false,
     },
