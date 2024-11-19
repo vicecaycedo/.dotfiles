bfdd96334beba9197dd5017d581c8e56412dbca1
Vicente Caycedo <vicentecaycedo@gmail.com>
2024-11-19 16:34:00 +0000

    Neovim: Remove telemetry setting that is now the default.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -42,9 +42,6 @@ return {
                 library = vim.api.nvim_get_runtime_file('', true),
                 checkThirdParty = false,
               },
-              telemetry = {
-                enable = false,
-              },
             },
           },
         })
