cca8870785c567640ee271bc90a2c5a4f32e3b9a
Vicente Caycedo <vicentecaycedo@gmail.com>
2025-02-16 11:32:26 -0500

    Neovim: Always hide parent directory in file tree.

--- nvim/.config/nvim/lua/plugins/file-tree.lua
+++ nvim/.config/nvim/lua/plugins/file-tree.lua
@@ -12,6 +12,9 @@ return {
       },
       view_options = {
         show_hidden = true,
+        is_always_hidden = function(name, _)
+          return name == '..'
+        end,
       },
     },
   },
