b3847cb7fd80f0fa9f8aefeaeeefcb06b17215a0
Vicente Caycedo <vicentecaycedo@gmail.com>
2025-02-19 10:11:46 -0500

    Neovim: Increase tab width.

--- nvim/.config/nvim/lua/vc/options.lua
+++ nvim/.config/nvim/lua/vc/options.lua
@@ -24,10 +24,10 @@ vim.o.list = true
 vim.opt.listchars = { tab = '» ', lead = '·', trail = '·', nbsp = '␣' }
 
 -- Tabs/indents.
-vim.o.shiftwidth = 2
+vim.o.shiftwidth = 4
 vim.o.smartindent = true
-vim.o.softtabstop = 2
-vim.o.tabstop = 2
+vim.o.softtabstop = 4
+vim.o.tabstop = 4
 
 -- Don't load built-in plugins.
 vim.g.loaded_gzip = true
