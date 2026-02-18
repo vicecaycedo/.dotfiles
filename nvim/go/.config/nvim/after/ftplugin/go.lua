ff175c65a76fa75a91dbcb3a8a5b2fc34a261a2a
Vicente Caycedo <vicentecaycedo@gmail.com>
2026-02-18 08:13:31 -0500

    Neovim: Make go opts local, not global.

--- nvim/go/.config/nvim/after/ftplugin/go.lua
+++ nvim/go/.config/nvim/after/ftplugin/go.lua
@@ -1,5 +1,5 @@
-vim.o.colorcolumn = '100'
-vim.o.expandtab = false
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
+vim.opt_local.colorcolumn = '100'
+vim.opt_local.expandtab = false
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
