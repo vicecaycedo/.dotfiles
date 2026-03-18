4e6e997aad0b576120368133a11ecdad93011479
Vicente Caycedo <vicentecaycedo@gmail.com>
2026-03-18 19:30:35 -0400

    Neovim: Add keymap for staging git hunks.

--- nvim/git/.config/nvim/lua/plugins/git.lua
+++ nvim/git/.config/nvim/lua/plugins/git.lua
@@ -10,5 +10,10 @@ return {
     end, 'previous diff')
     nmap('<Leader>md', require('gitsigns').preview_hunk_inline, 'hunk diff')
     nmap('<Leader>mu', require('gitsigns').reset_hunk, 'hunk undo')
+    vim.keymap.set('v', '<Leader>ms', function()
+      local start_line = vim.fn.line('.')
+      local end_line = vim.fn.line('v')
+      require('gitsigns').stage_hunk({ math.min(start_line, end_line), math.max(start_line, end_line) })
+    end, { desc = 'hunk stage' })
   end,
 }
